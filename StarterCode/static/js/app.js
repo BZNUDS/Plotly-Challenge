573456237ccc7bb42987ff4e9e7925f076620d2f
Brian Zdarsky <brian.zdarsky@gmail.com>
2022-04-11 23:17:34 -0500

    WIP

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,12 +9,19 @@ function init() {
         // Create an array of samples
         console.log("Does it call processCapsules???");
         processSamples(data)
+        var dropdown = d3.select("#selDataset");
+        data.names.forEach((name)=>{
+            dropdown.append("option").text(name).property("value",name);
+        })
+
     });
 }
 
 function optionChanged(value) {
     console.log("BZ printing in optionChanged");
     console.log(value);
+
+
 }
 
 
@@ -22,6 +29,14 @@ function processSamples(data) {
     console.log("BZ printing sample_id in processSamples function");
     sample_id = data.samples.map(d => d["id"]);
     console.log(sample_id);
+    dataset1=[]
+    dataset1=sample_id
+    console.log("BZ printing (dataset1=sample_id) in processSamples function");
+    console.log(dataset1);
+    ///console.log("BZ printing sample_id keys and values");
+    ///Object.keys(sample_id))
+    ///Object.sample_values(sample_id))
+
     console.log("BZ printing sample_values in processSamples function");
     sample_values = data.samples.map(d => d["sample_values"]);
     console.log(sample_values);
