fe588e7d26ee7c1f57502f1ce59986823b9acf80
Brian Zdarsky <brian.zdarsky@gmail.com>
2022-04-16 23:27:33 -0500

    WIP

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,9 +2,7 @@ function init() {
     d3.json("samples.json").then(data => {
         console.log("BZ printing data in init function");
         console.log(data);
-        console.log("BZ printing data.samples in init function");
         console.log(data.samples);
-        console.log("Does it call processCapsules???");
         processSamples(data)
         var dropdown = d3.select("#selDataset");
         console.log("Printing dropdown");
@@ -12,17 +10,22 @@ function init() {
         data.names.forEach((name)=>{
             dropdown.append("option").text(name).property("value",name);
         })
-    
+    // Call optionChanged to initalize
+    optionChanged('940')
     });
 }
 
-function optionChanged(value) {
-    // console.log("BZ printing name (passed in as value) in optionChanged");
-    // console.log(value);
+function display_metadata(meta_value) {
+
+
+}
+
+
+function optionChanged(value_in) {
     d3.json("samples.json").then(data => {
         // console.log("BZ printing data in optionChanged function");
         // console.log(data);
-        console.log("BZ printing data.samples in init function");
+        console.log("BZ printing data.samples in optionChanged function");
         console.log(data.samples);
         var samples = []
         samples=data.samples
@@ -37,14 +40,11 @@ function optionChanged(value) {
             dropdown.append("option").text(name).property("value",name);
             track_name[name] = [i];
             i += 1;
-            // console.log("Printing track_name[name]");
-            // console.log(track_name[name]);
-
         })
-        console.log("Printing track_name[value]");
-        console.log(track_name[value]);
-        console.log("Printing sample_selected aka samples[track_name[value]]");
-        sample_selected = samples[track_name[value]]
+        console.log("Printing track_name[value_in]");
+        console.log(track_name[value_in]);
+        console.log("Printing sample_selected aka samples[track_name[value_in]]");
+        sample_selected = samples[track_name[value_in]]
         console.log(sample_selected);
         console.log("Printing sample_selected.id");
         console.log(sample_selected.id);
@@ -68,20 +68,18 @@ function optionChanged(value) {
         var arr = [];
         do {
             result = result + j;
-            j += 1;
             // append new value to the array
             arr.push("Hola");
             bar_values.push(sample_selected.sample_values[j]);
             bar_labels.push(sample_selected.otu_ids[j]);
             bar_hovertext.push(sample_selected.otu_labels[j]);
-
+            j += 1;
         } while (j < 10);
         console.log(arr);
         console.log(sample_selected.sample_values);
         console.log(bar_values);
         console.log(bar_labels);
         console.log(bar_hovertext);
-
         console.log('Printing do/while loop result');
         console.log(result);
         // expected result: "12345"
@@ -99,24 +97,20 @@ function optionChanged(value) {
 
         // Slice the first 10 objects for plotting
         slicedData2 = sortedByGreekSearch2.slice(0, 10);
-        // console.log("Printing slicedData2");
-        // console.log(slicedData2);
 
         // Reverse the array to accommodate Plotly's defaults
         reversedData2 = slicedData2.reverse();
-        // console.log("Printing reversedData2");
-        // console.log(reversedData2);
-
             
         let bar_labels_str = bar_labels.map(convertAsString);
         // console.log("bar_labels_str:", bar_labels_str)
         reversedData = bar_values.reverse();
         console.log("reversedData:", reversedData)
         // bar_labels_str = ['944', '2419', '2539', '3450', '1795', '2389', '1314', '922', '1167', '2859'],
-        bar_labels_str = ['OTU_944', 'OTU_2419', 'OTU_2539', 'OTU_3450', 'OTU_1795', 'OTU_2389', 'OTU_1314', 'OYU_922', 'OTU_1167', 'OTU_2859'],
-        reversedLabels = bar_labels_str.reverse();
+        // bar_labels_str = ['OTU_944', 'OTU_2419', 'OTU_2539', 'OTU_3450', 'OTU_1795', 'OTU_2389', 'OTU_1314', 'OTU_922', 'OTU_1167', 'OTU_2859'],
+        bar_labels_str1 = bar_labels_str
+        bar_labels_str1=bar_labels_str1.map(obj => `OTU ${obj}`)
+        reversedLabels = bar_labels_str1.reverse();
         console.log("reversedLabels:", reversedLabels)
-        // reversedLabels[0] = "OTU "+reversedLabels[0]
         console.log("OTU reversedLabels[0]:", reversedLabels[0])
 
         let trace2 = {
@@ -124,18 +118,16 @@ function optionChanged(value) {
         y: reversedLabels,
         text: bar_hovertext,
         // name: "Greek",
-        marker: {color: 'red'},
+        marker: {color: 'light_blue'},
         type: "bar",
         orientation: "h"
         };
     
-            
         // Data array
         // `data` has already been defined, so we must choose a new name here:
         let traceData2 = [trace2];
         console.log("Printing traceData2");
         console.log(traceData2);
-
 
         // Apply a title to the layout
         let layout = {
@@ -153,9 +145,23 @@ function optionChanged(value) {
         Plotly.newPlot("bar", traceData2, layout);
 
         // Update buble chart
-        bubblely_chart(bar_labels, bar_values, bar_labels_str, bar_hovertext)
+        bubblely_chart(sample_selected.otu_ids, sample_selected.sample_values, sample_selected.otu_labels)
 
-
+        // Update demographic information in panel 
+        console.log("BZ printing data.metadata in optionChanged function");
+        console.log(data.metadata);
+        var display_metadata = data.metadata;
+        console.log("BZ printing display_metadata in optionChanged function");
+        console.log(data.metadata);
+        var metadata_array = display_metadata.filter((sampleobject) => sampleobject.id == value_in);
+        var md_a = metadata_array[0];
+        var panel_metadata = d3.select("#sample-metadata");
+        panel_metadata.html("");
+        Object.entries(md_a).forEach(([key,value]) => {
+            panel_metadata.append("h6").text('${key}: ${value}');
+            console.log("BZ printing key, value in optionChanged function");
+            console.log(key, value);
+        });
 
     });
 
@@ -169,14 +175,6 @@ function processSamples(data) {
     console.log("BZ printing sample_id in processSamples function");
     sample_id = data.samples.map(d => d["id"]);
     console.log(sample_id);
-    dataset1=[]
-    dataset1=sample_id
-    console.log("BZ printing (dataset1=sample_id) in processSamples function");
-    console.log(dataset1);
-    ///console.log("BZ printing sample_id keys and values");
-    ///Object.keys(sample_id))
-    ///Object.sample_values(sample_id))
-
     console.log("BZ printing sample_values in processSamples function");
     sample_values = data.samples.map(d => d["sample_values"]);
     console.log(sample_values);
@@ -188,22 +186,23 @@ function processSamples(data) {
     console.log(otu_labels);
 }
 
-function bubblely_chart(b_l, b_v, b_l_s, b_h){
-    // Passing in bar_labels, bar_values, bar_labels_str, bar_hovertext
-    // as b_l, b_v, b_l_s, b_h     
-    console.log("Printing b_l, b_v, b_l_s, b_h ", b_l, b_v, b_l_s, b_h);     
+function bubblely_chart(b_l, b_v, b_h){
+    // Passing in sample_selected.otu_ids, sample_selected.sample_values, sample_selected.otu_labels
+    // as b_l, b_v, b_h     
+    console.log("Printing b_l", b_l);     
+    console.log("Printing b_v", b_v);     
+    console.log("Printing b_h", b_h);
+
     var trace1 = {
-        // x: [1, 2, 3, 4],
-        // y: [10, 11, 12, 13],
         x: b_l,
         y: b_v,
-        // x: [944, 2419, 2539],
-        // y: [21, 24, 27],
+        text: b_h,
         mode: 'markers',
         marker: {
-          color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-          opacity: [1, 0.8, 0.6, 0.4],
-          size: [40, 60, 80, 100]
+        color: b_l,
+        // color: ['rgb(1167, 1167, 1167)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        // opacity: [1, 0.8, 0.6, 0.4],
+        size: b_v
         }
       };
       
@@ -213,7 +212,7 @@ function bubblely_chart(b_l, b_v, b_l_s, b_h){
         title: 'Marker Size and Color',
         showlegend: false,
         height: 600,
-        width: 600
+        width: 1400
       };
       
       Plotly.newPlot('bubble', data, layout);
